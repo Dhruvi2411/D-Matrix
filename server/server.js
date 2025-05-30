6e6d1540c7c31564fa90f3bc2c143a4d0bddf9bf
Dhrvui <mangaroliyadhruvi24@gmail.com>
2025-05-30 22:12:19 +0530

    Fix start script

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use("/api/contact", contactRoutes);
 app.use("/api/email", emailRoute);
 
 app.use(express.static(path.join(_dirname, "/client/dist")));
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
 });
 
